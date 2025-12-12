518d74ecce4ed87838d177e65641b6e45f589450
bussinessvideos587-create <bussinessvideos587@gmail.com>
2025-12-12 11:39:54 +0530

    Update cap.py

--- cap.py
+++ cap.py
@@ -12,11 +12,11 @@ logging.basicConfig(
 
 # --- Configuration ---
 # Get these from https://my.telegram.org
-API_ID = 1234567 
-API_HASH = "your_api_hash_here"
-BOT_TOKEN = "your_bot_token_here"
+API_ID = 35292658 
+API_HASH = "a14fdc9ed8e1456c9570381024954d0b"
+BOT_TOKEN = "8257600572:AAFVmlwEDiEy-AFzXN94XpUmsSRe6aXMxhw"
 
-app = Client("caption_recorder", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+app = Client("caption_recorder", api_id=35292658, api_hash="a14fdc9ed8e1456c9570381024954d0b", bot_token="8257600572:AAFVmlwEDiEy-AFzXN94XpUmsSRe6aXMxhw")
 
 # Store recorded captions and links: { chat_id: [(caption, link), ...] }
 channel_data = {}
